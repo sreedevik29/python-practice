5e58d678ba2b84e7aaa49be338dcdcd3d3fffd64
Sreedevi Krishnan <sreedevik29@gmail.com>
2017-06-28 14:57:27 -0400

    Attempt to use if and elif statements only

--- hangman-game/hangman.py
+++ hangman-game/hangman.py
@@ -3,52 +3,29 @@ from random import *
 def hangman():
 	print("I am thinking of an 8 letter word in NICKNAMES")
 	print("Guess a letter!")
-	
-	user_inputs = [
-		{
-			"user_input1": raw_input()
-		},
-		{
-			"user_input2": raw_input()
-		},
-		{
-			"user_input3": raw_input()
-		},
-		{
-			"user_input4": raw_input()
-		},
-		{
-			"user_input5": raw_input()
-		},
-		{
-			"user_input6": raw_input()
-		},
-		{
-			"user_input7": raw_input()
-		},
-		{
-			"user_input8": raw_input()
-		},
-		{
-			"user_input9": raw_input()
-		}
-	]
+	user_input1 = raw_input()
+	user_input2 = raw_input()
+	user_input3 = raw_input()
+	user_input4 = raw_input()
+	user_input5 = raw_input()
+	user_input6 = raw_input()
+	user_input7 = raw_input()
 
-	if user_inputs == "h":
+	if user_input1, user_input2, user_input3, user_input4, user_input5, user_input6, user_input7, user_input8 == "h":
 		print("You guessed a correct letter!")
-	elif user_inputs == "o":
-		print("You guessed a correct letter!")	
-	elif user_inputs == "n":
-		print("You guessed a correct letter!")	
-	elif user_inputs == "e":
-		print("You guessed a correct letter!")	
-	elif user_inputs == "y":
-		print("You guessed a correct letter!")	
-	elif user_inputs == "b":
-		print("You guessed a correct letter!")	
-	elif user_inputs == "u":
-		print("You guessed a correct letter!")	
-	elif user_inputs == "n":
+	elif user_input1, user_input2, user_input3, user_input4, user_input5, user_input6, user_input7, user_input8 == "o":
+		print("You guessed a correct letter!")		
+	elif user_input1, user_input2, user_input3, user_input4, user_input5, user_input6, user_input7, user_input8 == "n":
+		print("You guessed a correct letter!")		
+	elif user_input1, user_input2, user_input3, user_input4, user_input5, user_input6, user_input7, user_input8 == "e":
+		print("You guessed a correct letter!")		
+	elif user_input1, user_input2, user_input3, user_input4, user_input5, user_input6, user_input7, user_input8 == "y":
+		print("You guessed a correct letter!")		
+	elif user_input1, user_input2, user_input3, user_input4, user_input5, user_input6, user_input7, user_input8 == "b":
+		print("You guessed a correct letter!")		
+	elif user_input1, user_input2, user_input3, user_input4, user_input5, user_input6, user_input7, user_input8 == "u":
+		print("You guessed a correct letter!")		
+	elif user_input1, user_input2, user_input3, user_input4, user_input5, user_input6, user_input7, user_input8 == "n":
 		print("You guessed a correct letter!")		
 	else:
 		print("That's not the right letter!")	
