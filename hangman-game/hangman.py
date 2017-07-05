52ca3fde4ba8f929b2d8b0a2cbd82a34ec982876
Sreedevi Krishnan <sreedevik29@gmail.com>
2017-07-05 13:06:19 -0400

    Add another while loop to remove repeated letters

--- hangman-game/hangman.py
+++ hangman-game/hangman.py
@@ -14,16 +14,26 @@ def hangman():
 	print("I am thinking of an 8 letter word in NICKNAMES")
 	active = True
 	word = ["h", "o", "n", "e", "y", "b", "u", "n"]
+	# word = [ 
+	# 	{
+	# 	"h", "o", "n", "e", "y", "b", "u"
+	# 	},
+	# 	{
+	# 	"y", "o", "u", "s", "h", "i", "t", "e", "r"
+	# 	}
+	# ]
+	# select_word = random.choice(word)
 
 	while active:
 		user_input = raw_input("Guess the letter: ")
 		if user_input in word:
 			print("You guessed the right letter")
-			word.remove(user_input)
+			while user_input in word:
+				word.remove(user_input)
 		else: 
 			print("You guessed the wrong letter, try again")
 		if len(word) == 0:
-			print("Congrats you won!")
+			print("Congrats you won! The word was honeybun!")
 			active = False
 
 	print("Game is over!")
