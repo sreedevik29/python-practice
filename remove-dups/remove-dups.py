80e42dee404dbf072c169ca46009537d102ff6c0
Sreedevi Krishnan <sreedevik29@gmail.com>
2017-09-07 02:40:54 -0400

    Removed duplicates using set method

--- remove-dups/remove-dups.py
+++ remove-dups/remove-dups.py
@@ -1,14 +1,21 @@
-lists = ["droop", "stoop", "droop", "doop", "loop", "loop", "stoop", "foop"]
-newLists = []
 
-def appendList(lists,newLists):
-	for i in lists:
-		if i not in newLists:
-			newLists.append(i)
-		print(newLists)
+
+# def appendList(lists,newLists):
+# 	for i in lists:
+# 		if i not in newLists:
+# 			newLists.append(i)
+# 		print(newLists)
+
+# def main():
+# 	appendList(lists,newLists)
+
+def setList():
+	a = ["droop", "stoop", "droop", "doop", "loop", "loop", "stoop", "foop"]
+	c = list(set(a))
+	print(c)
 
 def main():
-	appendList(lists,newLists)
+	setList()
 
 if __name__ == '__main__':
 	main()
